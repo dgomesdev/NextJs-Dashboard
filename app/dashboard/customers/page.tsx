3c7caa8e4af08b53e02af18a6cbddc7836d7c347
dgomesdev <danilo.gomes@outlook.fr>
2024-02-22 18:43:49 -0400

    feat: added metadata

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,5 +1,14 @@
+import CustomersTable from '@/app/ui/customers/table';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
 export default function Page() {
-    return (
-        <p>Customers Page</p>
-    )
+  return (
+    <>
+    
+    </>
+  );
 }
